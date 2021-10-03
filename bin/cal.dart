29a0bb35c7f6d215951bb09beb53acab16122374
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2021-10-03 22:12:47 +0900

    Add files via upload

--- bin/cal.dart
+++ bin/cal.dart
@@ -1,55 +1,83 @@
-var callender = <int, num>{
-  1: 31,
-  2: 28,
-  3: 31,
-  4: 30,
-  5: 31,
-  6: 30,
-  7: 31,
-  8: 31,
-  9: 30,
-  10: 31,
-  11: 30,
-  12: 31
-};
+class Calculator {
+  var callender = <int, num>{
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+  };
 
-calculate(int year, int month, int date) {
-  var now = new DateTime.now();
+  Cal() { //if it is leap year, February is 29
+    var now = new DateTime.now();
 
-  num leftDay = 0;
-
-  if (year > now.year) {
-    //add dates of full year
-    int leftFullYear = year - now.year - 1;
-
-    leftDay += leftFullYear * 365;
-
-    //add dates of full month
-    int i = 1;
-    while (i < month) {
-      leftDay += (callender[i] as num);
-
-      i += 1;
+    if (now.year % 4 == 0 && now.year % 100 != 0) {
+      callender[2] = 29;
+    } else if (now.year % 400 == 0) {
+      callender[2] = 29;
     }
-
-    //add rest dates
-    leftDay += date;
-
-    //add rest dates of full month of this year
-    i = now.month + 1;
-    while (i < 13) {
-      leftDay += (callender[i] as num);
-
-      i += 1;
-    }
-
-    //add rest dates of this month
-    leftDay += (callender[now.month] as num) - now.day;
   }
 
-  return leftDay;
-}
+  calculate(int year, int month, int date) {
+    var now = new DateTime.now();
 
-main(List<String> args) {
-  print(calculate(2022, 7, 27));
+    num leftDay = 0;
+
+    if (year > now.year) { //not this year
+      //add dates of full year
+      int leftFullYear = year - now.year - 1;
+
+      leftDay += leftFullYear * 365;
+
+      //add dates of full month
+      int i = 1;
+      while (i < month) {
+        leftDay += (callender[i] as num);
+
+        i += 1;
+      }
+
+      //add rest dates
+      leftDay += date;
+
+      //add rest dates of full month of this year
+      i = now.month + 1;
+      while (i < 13) {
+        leftDay += (callender[i] as num);
+
+        i += 1;
+      }
+
+      //add rest dates of this month
+      leftDay += (callender[now.month] as num) - now.day;
+    } else { //this year
+      if (now.month < month) { //not this month
+        //add dates of full month
+        int i = now.month + 1;
+        while (i < month) {
+          leftDay += (callender[i] as num);
+
+          i += 1;
+        }
+
+        //add rest dates
+        leftDay += date;
+
+        //add rest dates of this month
+        leftDay += ((callender[i] as num) - now.day);
+
+        leftDay -= 1;
+      } else { //this month
+        leftDay = date - now.day;
+      }
+    }
+
+    return leftDay;
+  }
 }
